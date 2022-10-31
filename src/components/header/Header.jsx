9ebd1bd6efbd14df618e527c36e8c9dca74ab32e
Estebes Kamalidinov <estebeskam@gmail.com>
2022-10-31 16:44:07 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
             <div className="header__wrap container">
                 <div className="logo">
                     <img src={logo} alt="logo" />
-                    <Link to="/">Put'kaDB</Link>
+                    <Link to="/">tMovies</Link>
                 </div>
                 <ul className="header__nav">
                     {headerNav.map((e, i) => (
